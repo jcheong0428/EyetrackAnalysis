e4bb845c7f722f8fea3249aaaff3fbebe17c5f03
jcheong <jcheong0428@gmail.com>
2016-03-15 19:50:24 -0400

    added fourier example and playbutton function

--- EyetrackGrapher.m
+++ EyetrackGrapher.m
@@ -1,36 +1,41 @@
 function EyetrackGrapher(varargin)
 %EyetrackGrapher. Graphs the eyetracking data from Eyetribe
-%   EyetrackGrapher(FILENAME) Reads data from file FILENAME then plots the
-%   sampling rate, number of samples, and interpolation rate along with an
-%   interactive graph of gaze location, pupil size, and event, at each
-%   sample point. 
+%   Plots the sampling rate, number of samples, and interpolation rate 
+%   along with an interactive graph of gaze location, pupil size, and event, 
+%   at each sample point. 
 %
 % Example:
-%   EyetrackGrapher('/Users/jinhyuncheong/gamble_auto_2016-02-04_s03_r01.mat.tsv');
+%   dat = importfile;
+%   EyetrackGrapher(dat);
 %
-
-% for i = 1:length(d.imageArray)
-%      newmap=imresize(d.imageArray{i},.5,'nearest'); %need to resize because retina display issue
-%      imwrite(newmap,[num2str(d.timeArray(i)),'.png']);
-% end
-
 % Jin Hyun Cheong
 % Psychological Brain Sciences
 % Dartmouth College
-
-if length(varargin)==1
-    filename = varargin{1};
-else
+if length(varargin) < 1 % if no argument load default file
     dir = pwd;
     fn = 'regret_2016-03-15_s01_r01.mat.tsv';
     filename = [dir,'/data/test/regret_2016-03-15_s01_r01.mat.tsv'];
     filename2 = [dir,'/data/test/regret_2016-03-15_s01_r01.mat'];
+    [pre,post,intp,header]= importfile(filename);
+    load(filename2,'d');
+elseif length(varargin) == 1 % if an argument
+    if isstruct(varargin{1}) % if a struct data, use that
+        post = varargin{1}.post;
+        intp = varargin{1}.intp;
+        header = varargin{1}.header;
+        d = varargin{1}.d;
+    elseif ischar(varargin{1}) % if a string passed, load that filename
+        dir = pwd;
+        fn = varargin{1};
+        filename = [dir,'/data/test/',fn];
+        filename2 = [dir,'/data/test/',fn(1:end-4)];
+        [pre,post,intp,header]= importfile(filename);
+        load(filename2,'d');
+    end
 end    
-[pre,post,intp,header]= importfile(filename);
-load(filename2,'d');
 imts = (d.timeArray-d.timeArray(1))*1000;
 %%
-global xs ys zs event event_i d post intp; 
+global xs ys zs event event_i d post intp b bplay; 
 global f af zeta t1 t2 ax1 ax2 ax3 ax4 x1 x2 y1 y2 imts ts;
 warning('off','MATLAB:handle_graphics:exceptions:SceneNode')
 xs = post.avgx; % xcoordinate
@@ -52,7 +57,7 @@ f = figure;set(f,'Units','normalized','Position',[.1 .1 1 1]);
 %%
 plotbase();
 b = uicontrol('Parent',f,'Style','slider','Units','normalized','Position',[0.10 0 0.82 0.20],...
-              'value',zeta, 'min',0, 'max',length(xs),'SliderStep',[0.0005 0.01],'Callback',@b_callback);
+              'value',zeta, 'min',1, 'max',length(xs),'SliderStep',[1/length(xs) 0.01],'Callback',@b_callback);
 bgcolor = f.Color;
 bl1 = uicontrol('Parent',f,'Style','text','Units','normalized','Position',[0, 0.1, .2,.05],...
                 'String','0','BackgroundColor',bgcolor); % for min value
@@ -64,11 +69,9 @@ bl3 = uicontrol('Parent',f,'Style','text','Units','normalized','Position',[.4,.1
 p = uipanel(f,'Title','Data info',...
              'Position',[0.82 0.39  0.1 0.27]);
 b1 = uicontrol(p,'Style','pushbutton','String','Raw Data',...
-                'Units','normalized',...
-                'Position',[.1 .11 .8 .1],'Callback',@b1_callback);
+                'Units','normalized','Position',[.1 .11 .8 .1],'Callback',@b1_callback);
 b2 = uicontrol(p,'Style','pushbutton','String','Interpolated Data',...
-                'Units','normalized',...
-                'Position',[.1 0 .8 .1],'Callback',@b2_callback);
+                'Units','normalized','Position',[.1 0 .8 .1],'Callback',@b2_callback);
 b3 = uicontrol(p,'Style','text','Units','normalized','Position',[.1, .65, .8, .35],'HorizontalAlignment','left',...
                 'String',sprintf('File : %s',fn),'BackgroundColor',bgcolor); % Show file name
 b4 = uicontrol(p,'Style','text','Units','normalized','Position',[.1, .52, .8, .1], 'HorizontalAlignment','left',...
@@ -77,7 +80,9 @@ b5 = uicontrol(p,'Style','text','Units','normalized','Position',[.1, .4, .8, .1]
                 'String',sprintf('Samples : %d ',samples),'BackgroundColor',bgcolor); % Show number of samples
 b6 = uicontrol(p,'Style','text','Units','normalized','Position',[.1, .25, .8, .12],'HorizontalAlignment','left',...
                 'String',sprintf('Interpolation rate : %2.1f%%',interpolationrate),'BackgroundColor',bgcolor); % Show number of samples
-
+bplay = uicontrol('Parent',f,'Style','togglebutton','String','Play Stream',...
+                'Units','normalized',...
+                'Position',[.53,.12,.1,.05],'Callback',@bplay_callback);
 end
 function b_callback(es,ed)
     %% Left and right button functions are defined here.
@@ -129,6 +134,16 @@ zs = smooth(intp.psize); % pupil size
 event = intp.event_cell; % event cell 
 event_i = intp.event_ix;
 plotbase();
+end
+function bplay_callback(hObject, eventdata, handles)
+    global b zeta xs bplay
+    while get(bplay,'Value')
+        zeta = zeta+30;
+        set(b,'Value',zeta);
+        b_callback(b);
+        drawnow
+        WaitSecs(.1);
+    end
 end
 function plotbase()
 %%
